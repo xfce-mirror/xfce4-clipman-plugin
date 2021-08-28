23d43dce4fbf0ec7f89dfbba46cd547e6244b6a7
Sylvain Viart <sviart@cloudiway.com>
2021-08-28 07:47:55 +0200

    add shell wrapper script for pass
    test helper to experiment secure path usage

---
+++ pass_clip.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#
+
+DELETE_DELAY=30
+
+keep_password=0
+if [[ $1 == '-k' ]]
+then
+  keep_password=1
+  shift
+fi
+
+clipman_cli=~/code/xfce4-clipman-plugin/panel-plugin/clipman_cli.sh
+content=$(pass show "$1")
+if [[ -n $content ]]
+then
+  id=$($clipman_cli add -s "$(head -1 <<< "$content")" | awk '{print $2}')
+  unset content
+  if [[ -n $id ]]
+  then
+    echo -n "entry copied: $id"
+    if [[ $keep_password -eq 0 ]]
+    then
+      ( sleep $DELETE_DELAY && $clipman_cli del $id; ) &
+      disown $!
+      echo ", will be deleted in ${DELETE_DELAY}s"
+    else
+      echo ", will be kept"
+    fi
+    exit 0
+  fi
+fi
+
+# something went wrong
+exit 1
