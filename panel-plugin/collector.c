xfce-mirror/xfce4-clipman-plugin

commit abf2162375aa2296a2d169702af7a3764a65e80a

Add primary clipboard persistence
This adds a setting to make primary clipboard persistent over
deselection.

Closes: #31
MR: !2
Related: https://gitlab.gnome.org/GNOME/gtk/-/issues/317

panel-plugin/collector.c
@@ -42,6 +42,7 @@ struct _ClipmanCollectorPrivate
   guint                 primary_clipboard_timeout;
   gboolean              internal_change;
   gboolean              add_primary_clipboard;
+  gboolean              persistent_primary_clipboard;
   gboolean              history_ignore_primary_clipboard;
   gboolean              enable_actions;
   gboolean              inhibit;
@@ -52,6 +53,7 @@ G_DEFINE_TYPE_WITH_PRIVATE (ClipmanCollector, clipman_collector, G_TYPE_OBJECT)
 enum
 {
   ADD_PRIMARY_CLIPBOARD = 1,
+  PERSISTENT_PRIMARY_CLIPBOARD,
   HISTORY_IGNORE_PRIMARY_CLIPBOARD,
   ENABLE_ACTIONS,
   INHIBIT,
@@ -155,6 +157,7 @@ cb_clipboard_owner_change (ClipmanCollector *collector,
        * or if the shift key is hold down, and once both are released the
        * content will go to the history. */
       if (collector->priv->add_primary_clipboard
+          || collector->priv->persistent_primary_clipboard
           || !collector->priv->history_ignore_primary_clipboard
           || collector->priv->enable_actions)
         {
@@ -175,7 +178,15 @@ cb_request_text (GtkClipboard *clipboard,
   g_return_if_fail (GTK_IS_CLIPBOARD (collector->priv->default_clipboard) && GTK_IS_CLIPBOARD (collector->priv->primary_clipboard));
 
   if (text == NULL || text[0] == '\0')
-    return;
+    {
+      /* Restore primary clipboard on deselection */
+      if (clipboard == collector->priv->primary_clipboard
+          && collector->priv->persistent_primary_clipboard
+          && prev_text != NULL)
+        gtk_clipboard_set_text (collector->priv->primary_clipboard, prev_text, -1);
+
+      return;
+    }
 
   if (clipboard == collector->priv->default_clipboard)
     {
@@ -194,6 +205,13 @@ cb_request_text (GtkClipboard *clipboard,
       /* Make a copy inside the default clipboard */
       if (collector->priv->add_primary_clipboard)
         gtk_clipboard_set_text (collector->priv->default_clipboard, text, -1);
+
+      /* Store selection for later use */
+      if (collector->priv->persistent_primary_clipboard)
+        {
+          g_free (prev_text);
+          prev_text = g_strdup (text);
+        }
 
       /* Match for actions */
       if (collector->priv->enable_actions && g_strcmp0 (text, prev_text))
@@ -303,6 +321,13 @@ clipman_collector_class_init (ClipmanCollectorClass *klass)
                                                          DEFAULT_ADD_PRIMARY_CLIPBOARD,
                                                          G_PARAM_CONSTRUCT|G_PARAM_READWRITE));
 
+  g_object_class_install_property (object_class, PERSISTENT_PRIMARY_CLIPBOARD,
+                                   g_param_spec_boolean ("persistent-primary-clipboard",
+                                                         "PersistentPrimaryClipboard",
+                                                         "Make the primary clipboard persistent over deselection",
+                                                         DEFAULT_PERSISTENT_PRIMARY_CLIPBOARD,
+                                                         G_PARAM_CONSTRUCT|G_PARAM_READWRITE));
+
   g_object_class_install_property (object_class, HISTORY_IGNORE_PRIMARY_CLIPBOARD,
                                    g_param_spec_boolean ("history-ignore-primary-clipboard",
                                                          "HistoryIgnorePrimaryClipboard",
@@ -377,6 +402,10 @@ clipman_collector_set_property (GObject *object,
       priv->add_primary_clipboard = g_value_get_boolean (value);
       break;
 
+    case PERSISTENT_PRIMARY_CLIPBOARD:
+      priv->persistent_primary_clipboard = g_value_get_boolean (value);
+      break;
+
     case HISTORY_IGNORE_PRIMARY_CLIPBOARD:
       priv->history_ignore_primary_clipboard = g_value_get_boolean (value);
       break;
@@ -406,6 +435,10 @@ clipman_collector_get_property (GObject *object,
     {
     case ADD_PRIMARY_CLIPBOARD:
       g_value_set_boolean (value, priv->add_primary_clipboard);
+      break;
+
+    case PERSISTENT_PRIMARY_CLIPBOARD:
+      g_value_set_boolean (value, priv->persistent_primary_clipboard);
       break;
 
     case HISTORY_IGNORE_PRIMARY_CLIPBOARD:

panel-plugin/common.h
@@ -34,6 +34,7 @@
 #define DEFAULT_REORDER_ITEMS                           TRUE
 #define DEFAULT_SKIP_ACTION_ON_KEY_DOWN                 FALSE
 #define DEFAULT_ADD_PRIMARY_CLIPBOARD                   FALSE
+#define DEFAULT_PERSISTENT_PRIMARY_CLIPBOARD            FALSE
 #define DEFAULT_SHOW_QR_CODE                            FALSE
 #define DEFAULT_HISTORY_IGNORE_PRIMARY_CLIPBOARD        TRUE
 #define DEFAULT_ENABLE_ACTIONS                          FALSE

panel-plugin/plugin.c
@@ -125,6 +125,8 @@ plugin_register (gboolean panel_plugin)
   plugin->collector = clipman_collector_get ();
   xfconf_g_property_bind (plugin->channel, "/settings/add-primary-clipboard",
                           G_TYPE_BOOLEAN, plugin->collector, "add-primary-clipboard");
+  xfconf_g_property_bind (plugin->channel, "/settings/persistent-primary-clipboard",
+                          G_TYPE_BOOLEAN, plugin->collector, "persistent-primary-clipboard");
   xfconf_g_property_bind (plugin->channel, "/settings/history-ignore-primary-clipboard",
                           G_TYPE_BOOLEAN, plugin->collector, "history-ignore-primary-clipboard");
   xfconf_g_property_bind (plugin->channel, "/settings/enable-actions",

panel-plugin/settings-dialog.ui
@@ -130,6 +130,23 @@
                               </packing>
                             </child>
                             <child>
+                              <object class="GtkCheckButton" id="persistent-selections">
+                                <property name="label" translatable="yes">_Persist primary selection</property>
+                                <property name="visible">True</property>
+                                <property name="can_focus">True</property>
+                                <property name="receives_default">False</property>
+                                <property name="has_tooltip">True</property>
+                                <property name="tooltip_text" translatable="yes">If checked, the middle-click paste (primary selection) will be persistent if the next selection is empty, i.e. left click anywhere in a text area</property>
+                                <property name="use_underline">True</property>
+                                <property name="draw_indicator">True</property>
+                              </object>
+                              <packing>
+                                <property name="expand">False</property>
+                                <property name="fill">True</property>
+                                <property name="position">1</property>
+                              </packing>
+                            </child>
+                            <child>
                               <object class="GtkCheckButton" id="show-qr-code">
                                 <property name="label" translatable="yes">_QR-Code support</property>
                                 <property name="visible">True</property>
@@ -143,7 +160,7 @@
                               <packing>
                                 <property name="expand">False</property>
                                 <property name="fill">True</property>
-                                <property name="position">1</property>
+                                <property name="position">2</property>
                               </packing>
                             </child>
                             <child>
@@ -182,7 +199,7 @@
                               <packing>
                                 <property name="expand">False</property>
                                 <property name="fill">False</property>
-                                <property name="position">2</property>
+                                <property name="position">3</property>
                               </packing>
                             </child>
                           </object>

panel-plugin/xfce4-clipman-settings.c
@@ -83,6 +83,8 @@ prop_dialog_run (void)
   /* Behavior tab: General */
   gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (gtk_builder_get_object (builder, "add-selections")),
                                 DEFAULT_ADD_PRIMARY_CLIPBOARD);
+  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (gtk_builder_get_object (builder, "persistent-selections")),
+                                DEFAULT_PERSISTENT_PRIMARY_CLIPBOARD);
 #ifdef HAVE_QRENCODE
   gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (gtk_builder_get_object (builder, "show-qr-code")),
                                 DEFAULT_SHOW_QR_CODE);
@@ -105,6 +107,8 @@ prop_dialog_run (void)
 #endif
   xfconf_g_property_bind (xfconf_channel, "/settings/add-primary-clipboard", G_TYPE_BOOLEAN,
                           gtk_builder_get_object (builder, "add-selections"), "active");
+  xfconf_g_property_bind (xfconf_channel, "/settings/persistent-primary-clipboard", G_TYPE_BOOLEAN,
+                          gtk_builder_get_object (builder, "persistent-selections"), "active");
   xfconf_g_property_bind (xfconf_channel, "/tweaks/paste-on-activate",
                           G_TYPE_UINT, G_OBJECT (combobox), "active");
 

commit 8e1aa51bcf427c39443c5538255f1810d732a4a9

collector: Make actions work again for selections
Regression introduced in abf2162375aa2296a2d169702af7a3764a65e80a.

Related: !2

panel-plugin/collector.c
@@ -206,17 +206,16 @@ cb_request_text (GtkClipboard *clipboard,
       if (collector->priv->add_primary_clipboard)
         gtk_clipboard_set_text (collector->priv->default_clipboard, text, -1);
 
-      /* Store selection for later use */
-      if (collector->priv->persistent_primary_clipboard)
+      /* Match for actions */
+      if (collector->priv->enable_actions && g_strcmp0 (text, prev_text) != 0)
         {
+          clipman_actions_match_with_menu (collector->priv->actions, ACTION_GROUP_SELECTION, text);
           g_free (prev_text);
           prev_text = g_strdup (text);
         }
-
-      /* Match for actions */
-      if (collector->priv->enable_actions && g_strcmp0 (text, prev_text))
+      /* Store selection for later use */
+      else if (collector->priv->persistent_primary_clipboard)
         {
-          clipman_actions_match_with_menu (collector->priv->actions, ACTION_GROUP_SELECTION, text);
           g_free (prev_text);
           prev_text = g_strdup (text);
         }