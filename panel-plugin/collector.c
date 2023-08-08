xfce-mirror/xfce4-clipman-plugin

commit eab4beaca2584010616439e2d83808bb965f3e8a

Move default clipboard restoration from manager to collector
In a simplified form, where only text is restored, similar to what is
done for the primary clipboard. This ensures the same behavior, whether
Clipman's clipboard manager is used or not (#28). It also allows all
restoration operations to be carried out in a single place, for greater
control, especially when the history is cleared (#4).

Fixes: #4, #28

panel-plugin/collector.c
@@ -39,6 +39,8 @@ struct _ClipmanCollectorPrivate
   ClipmanHistory       *history;
   GtkClipboard         *default_clipboard;
   GtkClipboard         *primary_clipboard;
+  gchar                *default_cache;
+  gchar                *primary_cache;
   guint                 primary_clipboard_timeout;
   gboolean              default_internal_change;
   gboolean              primary_internal_change;
@@ -187,30 +189,27 @@ cb_request_text (GtkClipboard *clipboard,
                  const gchar *text,
                  ClipmanCollector *collector)
 {
-  static gchar *prev_text = NULL;
-
   g_return_if_fail (GTK_IS_CLIPBOARD (collector->priv->default_clipboard) && GTK_IS_CLIPBOARD (collector->priv->primary_clipboard));
 
   if (text == NULL)
     {
       /* Restore primary clipboard on deselection */
-      if (clipboard == collector->priv->primary_clipboard && prev_text != NULL && (
+      if (clipboard == collector->priv->primary_clipboard && collector->priv->primary_cache != NULL && (
             (collector->priv->persistent_primary_clipboard && !collector->priv->add_primary_clipboard)
             || (collector->priv->add_primary_clipboard
                 && gtk_clipboard_wait_is_text_available (collector->priv->default_clipboard))
          ))
         {
           collector->priv->primary_internal_change = TRUE;
-          gtk_clipboard_set_text (collector->priv->primary_clipboard, prev_text, -1);
+          gtk_clipboard_set_text (collector->priv->primary_clipboard, collector->priv->primary_cache, -1);
         }
 
-      /* Clear primary clipboard if default clipboard was cleared */
-      if (clipboard == collector->priv->default_clipboard
-          && collector->priv->add_primary_clipboard)
+      /* Restore default clipboard when cleared unintentionally (if this was intentional,
+       * 'default_cache' should have been cleared before) */
+      if (clipboard == collector->priv->default_clipboard && collector->priv->default_cache != NULL)
         {
-          collector->priv->primary_internal_change = TRUE;
-          gtk_clipboard_set_text (collector->priv->primary_clipboard, "", -1);
-          gtk_clipboard_clear (collector->priv->primary_clipboard);
+          collector->priv->default_internal_change = TRUE;
+          gtk_clipboard_set_text (collector->priv->default_clipboard, collector->priv->default_cache, -1);
         }
 
       return;
@@ -222,15 +221,20 @@ cb_request_text (GtkClipboard *clipboard,
       clipman_history_add_text (collector->priv->history, text);
 
       /* Make a copy inside the primary clipboard */
-      if (collector->priv->add_primary_clipboard && g_strcmp0 (text, prev_text) != 0)
+      if (collector->priv->add_primary_clipboard && g_strcmp0 (text, collector->priv->primary_cache) != 0)
         {
           collector->priv->primary_internal_change = TRUE;
           gtk_clipboard_set_text (collector->priv->primary_clipboard, text, -1);
+          g_free (collector->priv->primary_cache);
+          collector->priv->primary_cache = g_strdup (text);
         }
 
       /* Match for actions */
       if (collector->priv->enable_actions)
         clipman_actions_match_with_menu (collector->priv->actions, ACTION_GROUP_MANUAL, text);
+
+      g_free (collector->priv->default_cache);
+      collector->priv->default_cache = g_strdup (text);
     }
   else if (clipboard == collector->priv->primary_clipboard)
     {
@@ -239,10 +243,12 @@ cb_request_text (GtkClipboard *clipboard,
         clipman_history_add_text (collector->priv->history, text);
 
       /* Make a copy inside the default clipboard */
-      if (collector->priv->add_primary_clipboard)
+      if (collector->priv->add_primary_clipboard && g_strcmp0 (text, collector->priv->default_cache) != 0)
         {
           collector->priv->default_internal_change = TRUE;
           gtk_clipboard_set_text (collector->priv->default_clipboard, text, -1);
+          g_free (collector->priv->default_cache);
+          collector->priv->default_cache = g_strdup (text);
         }
 
       /* Match for actions */
@@ -252,8 +258,8 @@ cb_request_text (GtkClipboard *clipboard,
       /* Store selection for later use */
       if (collector->priv->persistent_primary_clipboard || collector->priv->add_primary_clipboard)
         {
-          g_free (prev_text);
-          prev_text = g_strdup (text);
+          g_free (collector->priv->primary_cache);
+          collector->priv->primary_cache = g_strdup (text);
         }
     }
 }
@@ -279,6 +285,15 @@ clipman_collector_set_is_restoring (ClipmanCollector *collector,
     collector->priv->default_internal_change = TRUE;
   else if (clipboard == collector->priv->primary_clipboard)
     collector->priv->primary_internal_change = TRUE;
+}
+
+void
+clipman_collector_clear_cache (ClipmanCollector *collector)
+{
+  g_free (collector->priv->default_cache);
+  g_free (collector->priv->primary_cache);
+  collector->priv->default_cache = NULL;
+  collector->priv->primary_cache = NULL;
 }
 
 ClipmanCollector *
@@ -394,6 +409,7 @@ clipman_collector_finalize (GObject *object)
     }
   g_object_unref (collector->priv->actions);
   g_object_unref (collector->priv->history);
+  clipman_collector_clear_cache (collector);
 }
 
 static void

panel-plugin/collector.h
@@ -53,6 +53,7 @@ GType                   clipman_collector_get_type              (void);
 ClipmanCollector *      clipman_collector_get                   (void);
 void                    clipman_collector_set_is_restoring      (ClipmanCollector *collector,
                                                                  GtkClipboard     *clipboard);
+void                    clipman_collector_clear_cache           (ClipmanCollector *collector);
 
 #endif /* !__CLIPMAN_COLLECTOR_H__ */
 

panel-plugin/menu.c
@@ -268,16 +268,17 @@ cb_clear_history (ClipmanMenu *menu)
 
   clipman_history_clear (menu->priv->history);
 
+  /* prevent persistent-primary-clipboard from restoring the selection and do not
+   * restore default clipboard either */
+  collector = clipman_collector_get ();
+  clipman_collector_clear_cache (collector);
+  g_object_unref (collector);
+
   clipboard = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
   gtk_clipboard_set_text (clipboard, "", -1);
   gtk_clipboard_clear (clipboard);
 
-  /* prevent persistent-primary-clipboard from restoring the selection */
-  collector = clipman_collector_get ();
   clipboard = gtk_clipboard_get (GDK_SELECTION_PRIMARY);
-  clipman_collector_set_is_restoring (collector, clipboard);
-  g_object_unref (collector);
-
   gtk_clipboard_set_text (clipboard, "", -1);
   gtk_clipboard_clear (clipboard);
 }

x11-clipboard-manager/gsd-clipboard-manager.c
@@ -310,8 +310,17 @@ start_clipboard_idle_cb (gpointer user_data)
         XSelectInput (display, window, PropertyChangeMask);
         XSetSelectionOwner (display, XA_CLIPBOARD_MANAGER, window, timestamp);
 
+        /*
+         * A weaker version of this (text-only restore) has been added to ClipmanCollector
+         * instead, as of 1.6.4. In this way, all restore operations are grouped together
+         * in the same place in the code, and Clipman's behavior should no longer depend
+         * on the clipboard manager used (this one or the xfsettingsd one).
+         * For the same reason as below, this code is currently disabled rather than deleted.
+         */
+#if 0
         g_signal_connect_swapped (manager->priv->default_clipboard, "owner-change",
                                   G_CALLBACK (default_clipboard_owner_change), manager);
+#endif
         /*
          * This duplicates and may conflict with the persistent-primary-clipboard option
          * implemented in ClipmanCollector since 1.6.3, but the logic here is a little

commit 0124079caa1507d32a071a26538e7ec8745c2fa8

collector: Do not restore default clipboard if not really empty
Closes: #85
Fixes: eab4beaca2584010616439e2d83808bb965f3e8a

panel-plugin/collector.c
@@ -208,8 +208,19 @@ cb_request_text (GtkClipboard *clipboard,
        * 'default_cache' should have been cleared before) */
       if (clipboard == collector->priv->default_clipboard && collector->priv->default_cache != NULL)
         {
-          collector->priv->default_internal_change = TRUE;
-          gtk_clipboard_set_text (collector->priv->default_clipboard, collector->priv->default_cache, -1);
+          GdkAtom *targets;
+          gint n_targets;
+          if (!gtk_clipboard_wait_for_targets (collector->priv->default_clipboard, &targets, &n_targets))
+            {
+              collector->priv->default_internal_change = TRUE;
+              gtk_clipboard_set_text (collector->priv->default_clipboard, collector->priv->default_cache, -1);
+            }
+          else
+            {
+              /* text was reset but clipboard contains some data (e.g. objects in FreeCAD,
+               * see https://gitlab.xfce.org/panel-plugins/xfce4-clipman-plugin/-/issues/85) */
+              g_free (targets);
+            }
         }
 
       return;