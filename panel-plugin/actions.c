xfce-mirror/xfce4-clipman-plugin

commit 65d89e76f3579ed7907c23cf9a2eac50e0d8bd80

actions: Save changes after closing action dialog if necessary
Fixes: #128

panel-plugin/xfce4-clipman-settings.c
@@ -407,6 +407,8 @@ apply_action (const gchar *original_action_name)
   /* Refresh the actions treeview */
   treeview = GTK_WIDGET (gtk_builder_get_object (builder, "actions"));
   refresh_actions_treeview (GTK_TREE_VIEW (treeview));
+
+  clipman_actions_save (actions);
 }
 
 static void
@@ -918,7 +920,6 @@ static void
 shutdown (GApplication *app,
           gpointer user_data)
 {
-  clipman_actions_save (actions);
   g_object_unref (actions);
   g_object_unref (builder);
   g_object_unref (xfconf_channel);

commit 6b0c6c4f5be066d07420d60fcf37ad8520d027bc

actions: Block file monitoring from settings dialog
Simplest way to avoid segfault because of freed data, as we don't need
file monitoring here. Also, save actions when one of them is deleted.

Related: https://gitlab.xfce.org/panel-plugins/xfce4-clipman-plugin/-/issues/128#note_117750
Fixes: 65d89e76f3579ed7907c23cf9a2eac50e0d8bd80

panel-plugin/actions.c
@@ -49,6 +49,7 @@ struct _ClipmanActionsPrivate
   GSList *entries;
   GtkWidget *menu;
   gboolean skip_action_on_key_down;
+  gboolean block_monitoring;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE (ClipmanActions, clipman_actions, G_TYPE_OBJECT)
@@ -393,6 +394,9 @@ cb_file_changed (ClipmanActions *actions,
 {
   static GSource *source = NULL;
   guint source_id;
+
+  if (actions->priv->block_monitoring)
+    return;
 
   if (event_type == G_FILE_MONITOR_EVENT_CHANGES_DONE_HINT)
     {
@@ -930,6 +934,12 @@ clipman_actions_get (void)
     g_object_ref (G_OBJECT (singleton));
 
   return singleton;
+}
+
+void
+clipman_actions_block_monitoring (ClipmanActions *actions)
+{
+  actions->priv->block_monitoring = TRUE;
 }
 
 GRegex *

panel-plugin/actions.h
@@ -102,6 +102,8 @@ void
 clipman_actions_load (ClipmanActions *actions);
 void
 clipman_actions_save (ClipmanActions *actions);
+void
+clipman_actions_block_monitoring (ClipmanActions *actions);
 GRegex *
 clipman_actions_get_regex (const gchar *pattern);
 

panel-plugin/xfce4-clipman-settings.c
@@ -530,6 +530,8 @@ cb_delete_action (GtkButton *button)
   gtk_tree_model_get (model, &iter, 0, &entry, -1);
   clipman_actions_remove (actions, entry->action_name);
   gtk_list_store_remove (GTK_LIST_STORE (model), &iter);
+
+  clipman_actions_save (actions);
 }
 
 static void
@@ -557,6 +559,7 @@ cb_reset_actions (GtkButton *button)
 
   g_object_unref (actions);
   actions = clipman_actions_get ();
+  clipman_actions_block_monitoring (actions);
   refresh_actions_treeview (GTK_TREE_VIEW (gtk_builder_get_object (builder, "actions")));
 }
 
@@ -962,6 +965,7 @@ command_line (GApplication *app,
   g_signal_connect (app, "shutdown", G_CALLBACK (shutdown), NULL);
   xfconf_channel = xfconf_channel_new_with_property_base ("xfce4-panel", "/plugins/clipman");
   actions = clipman_actions_get ();
+  clipman_actions_block_monitoring (actions);
   prop_dialog_init ();
   gtk_window_set_application (GTK_WINDOW (settings_dialog), GTK_APPLICATION (app));
   gtk_widget_show (settings_dialog);